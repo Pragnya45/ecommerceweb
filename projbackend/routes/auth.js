9bb23aa28ddfdbac71e8842421434ab32cc90a38
BasudevBharatBhushan <bharatbhushan173@gmail.com>
2023-04-15 12:19:19 +0530

    pull check

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,40 +1,36 @@
-
+// git pull check bharat
 
 var express = require("express");
 var router = express.Router();
-const { check, validationResult } = require('express-validator');
-const {signout,signup,signin,isSignedIn} = require("../controllers/auth");
+const { check, validationResult } = require("express-validator");
+const { signout, signup, signin, isSignedIn } = require("../controllers/auth");
 
-
-router.post("/signup",
-[
-  //Express-validator
-  check("name", "name should be at least 3 char").isLength({ min: 3 }),
-  check("email", "email is required").isEmail(),
-  check("password", "password should be at least 3 char").isLength({
-  min: 3,
- }),
-],
-signup //controller
+router.post(
+  "/signup",
+  [
+    //Express-validator
+    check("name", "name should be at least 3 char").isLength({ min: 3 }),
+    check("email", "email is required").isEmail(),
+    check("password", "password should be at least 3 char").isLength({
+      min: 3,
+    }),
+  ],
+  signup //controller
 );
 
+router.post("/signin", [
+  check("email").isEmail().withMessage("email is required"),
+  check("password", "password field is required").isLength({
+    min: 1,
+  }),
 
-router.post("/signin",[
-
-    check("email").isEmail().withMessage("email is required"),
-    check("password", "password field is required").isLength({
-        min: 1,
-      }),
-  
-     signin
+  signin,
 ]);
 
+router.get("/signout", signout);
 
-
-router.get("/signout",signout);
-
-router.get("/testroute",isSignedIn,(req,res)=>{
-    res.json(req.auth);
+router.get("/testroute", isSignedIn, (req, res) => {
+  res.json(req.auth);
 });
 
 module.exports = router;
