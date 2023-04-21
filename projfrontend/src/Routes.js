a33c17a8d5db6405c20d4bdb02ed43dbe743409a
lonewolf <pragnya330@gmail.com>
2023-04-21 14:51:19 +0530

    sign up completed

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -1,14 +1,28 @@
 import React from "react";
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
-import Home from "./core/Home"
+import Home from "./core/Home";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
+import AdminRoute from "./auth/helper/AdminRoutes";
+import PrivateRoute from "./auth/helper/PrivateRoutes";
+import UserDashBoard from "./user/UserDashBoard";
+import AdminDashBoard from "./user/AdminDashBoard";
+import AddCategory from "./admin/AddCategory";
+
 const  Routes = () =>  {
   return (
     <BrowserRouter>
     <Switch>
-      <Route path="/" exact component={Home} />
+      
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="/signin" element={<Signin/>} />
+      <PrivateRoute path="/user/dashboard" element={<UserDashBoard/>} /> 
+      <AdminRoute path="/admin/dashboard" element={<AdminDashBoard/>} /> 
+      <AdminRoute path="/admin/create/category" element={<AddCategory/>} /> 
     </Switch>
     </BrowserRouter>
-  )
+  );
 }
 
 export default Routes;
--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import "../styles.css";
+import{ API }from "../backend";
+import Base from"./Base";
+
 
 export default function Home() {
+  console.log("API IS", API);
   return (
-    <div>
-      <h1>hello front end</h1>
-    </div>
-  )
+    <Base title="Home Page" description="Welcome to the Tshirt store">
+      <div className="row">
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+        <div className="col-4">
+        <button className="btn btn-success">TEST</button>
+        </div>
+        <div className="col-4">
+        <button className="btn btn-success">TEST</button>
+        </div>
+      </div>
+    </Base>
+  );
 }
