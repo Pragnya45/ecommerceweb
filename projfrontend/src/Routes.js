3100f62f6e8190e02bf0c437ef32aba6d37670e4
BasudevBharatBhushan <bharatbhushan173@gmail.com>
2023-04-21 15:25:48 +0530

    sigin completed

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -9,20 +9,31 @@ import UserDashBoard from "./user/UserDashBoard";
 import AdminDashBoard from "./user/AdminDashBoard";
 import AddCategory from "./admin/AddCategory";
 
-const  Routes = () =>  {
+const Routes = () => {
   return (
     <BrowserRouter>
-    <Switch>
-      
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup/>} />
-      <Route path="/signin" element={<Signin/>} />
-      <PrivateRoute path="/user/dashboard" element={<UserDashBoard/>} /> 
-      <AdminRoute path="/admin/dashboard" element={<AdminDashBoard/>} /> 
-      <AdminRoute path="/admin/create/category" element={<AddCategory/>} /> 
-    </Switch>
+      <Switch>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+
+        <Route exact path="/user/dashboard" element={<PrivateRoute />}>
+          <Route exact path="/user/dashboard" element={<UserDashBoard />} />
+        </Route>
+        <Route exact path="/admin/dashboard" element={<PrivateRoute />}>
+          <Route exact path="/admin/dashboard" element={<AdminDashBoard />} />
+        </Route>
+
+        <Route exact path="/admin/create/category" element={<PrivateRoute />}>
+          <Route
+            exact
+            path="/admin/create/category"
+            element={<AddCategory />}
+          />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
-}
+};
 
 export default Routes;
