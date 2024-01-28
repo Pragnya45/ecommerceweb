807aebce8480d239cedfe0cf1fd5f528a21f7968
Pragnya sahu <pragnya110@gmail.com>
2024-01-28 18:46:21 +0530

    auth changes

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -29,19 +29,56 @@ exports.signup = (req, res) => {
   });
 };
 
-exports.signin = (req, res) => {
-  const errors = validationResult(req);
-  const { email, password } = req.body; //destructuring code
+// exports.signin = (req, res) => {
+//   const errors = validationResult(req);
+//   const { email, password } = req.body; //destructuring code
 
-  if (!errors.isEmpty()) {
-    ///validation error
-    return res.status(422).json({
-      error: errors.array()[0].msg,
-    });
-  }
+//   if (!errors.isEmpty()) {
+//     ///validation error
+//     return res.status(422).json({
+//       error: errors.array()[0].msg,
+//     });
+//   }
+//   // User.findOne({ email }, (err, user) => {
+//   //   if (err || !user) {
+//   //     return res.status(400).json({
+//   //       error: "User email does not exist",
+//   //     });
+//   //   }
 
-  User.findOne({ email }, (err, user) => {
-    if (err || !user) {
+//   //   if (!user.authenticate(password)) {
+//   //     return res.status(401).json({
+//   //       error: "Email and password do not match",
+//   //     });
+//   //   }
+
+//   //   //create token
+//   //   const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+//   //   //put token in cookie
+//   //   res.cookie("token", token, { expire: new Date() + 9999 });
+
+//   //   //send response to front end
+//   //   const { _id, name, email, role } = user;
+//   //   return res.json({ token, user: { _id, name, email, role } });
+//   // });
+// };
+
+exports.signin = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    const { email, password } = req.body; // Destructuring code
+
+    if (!errors.isEmpty()) {
+      // Validation error
+      return res.status(422).json({
+        error: errors.array()[0].msg,
+      });
+    }
+
+    const user = await User.findOne({ email });
+    console.log(user);
+
+    if (!user) {
       return res.status(400).json({
         error: "User email does not exist",
       });
@@ -53,16 +90,21 @@ exports.signin = (req, res) => {
       });
     }
 
-    //create token
+    // Create token
     const token = jwt.sign({ _id: user._id }, process.env.SECRET);
-    //put token in cookie
+
+    // Put token in cookie
     res.cookie("token", token, { expire: new Date() + 9999 });
 
-    //send response to front end
-    const { _id, name, email, role } = user;
+    // Send response to frontend
+    const { _id, name, role } = user;
     return res.json({ token, user: { _id, name, email, role } });
-  });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
+
 exports.signout = (req, res) => {
   res.clearCookie("token");
   res.json({
@@ -91,12 +133,11 @@ exports.isAuthenticated = (req, res, next) => {
 
 exports.isAdmin = (req, res, next) => {
   if (req.profile.role === 0) {
-    
     //role of profile is 0 then he is not admin if 1 then admin.
     return res.status(403).json({
       error: "You are not ADMIN,Access denied",
     });
   }
-  
+
   next();
 };
